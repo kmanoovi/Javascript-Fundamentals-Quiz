31a8971316502f7694080366f98ef01655a87494
kmanoovi <ktmandovi@gmail.com>
2022-09-05 15:30:43 -0400

    added questions and started css file

--- assets/script.js
+++ assets/script.js
@@ -16,5 +16,16 @@ function setTime() {
 
 
 // addButton.addEventListener("click", function() {
-//     if (count < )
-// })
+//     if ();
+// }
+// );
+function generateQuiz() {
+var questionOne = window.prompt("Javascript is a dynamic scripting language that enhances the user experience. Yes or No.");
+var questionTwo = window.prompt("You DO NOT use console logs to help explore your code. Yes or No.");
+var questionThree = window.prompt("An array is a variable with a list of values. Yes or No.");
+var questionFour = window.prompt("A 'for loop' executes a block of code so long as the condition of the statement is true. Yes or No.");
+var questionFive = window.prompt("The 'this' keyword is not confusing at all. Yes or No.");
+
+// For Loops //
+
+}
