cc6119168b334f45cca15228d3f13c4db94f096b
kmanoovi <ktmandovi@gmail.com>
2022-09-08 16:58:45 -0400

    added to README doc

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Javascript-Fundamentals-Quiz
+
+This quiz asks questions to test the Javascript knowledge of the user.
+
+A basic HTML document was created as well as a CSS document. Most of the coding work
+is inside of the JS document.
+
+I started by naming the variables I knew I would be using. Then moved onto writing 
+Y/N questions about Javascript.
+
+For Loops were created so that the webpage knew what to do if a question was
+answered incorrectly.
+
+I used the local storage to record initials and the score of the user.
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,37 @@
 var timeEl = document.querySelector (".time");
 var mainEl = document.getElementById("main");
 var secondsLeft = 180;
+var startBttn = document.getElementById("#start");
+var saveBttn = document.getElementById("#save");
+var initials = localStorage.getItem("initials");
+var score = localStorage.getItem("score");
+
+function generateQuiz() {
+    var questionOne = window.prompt("Javascript is a dynamic scripting language that enhances the user experience. Yes or No.");
+    var questionTwo = window.prompt("You DO NOT use console logs to help explore your code. Yes or No.");
+    var questionThree = window.prompt("An array is a variable with a list of values. Yes or No.");
+    var questionFour = window.prompt("A 'for loop' executes a block of code so long as the condition of the statement is true. Yes or No.");
+    var questionFive = window.prompt("The 'this' keyword is not confusing at all. Yes or No.");
+
+// For Loops //
+    if (questionOne == true) {
+    } else {(setTime--);}
+
+    if (questionTwo == true) {
+    } else {(setTime--);}
+
+    if (questionThree == true) {
+    } else {(setTime--);}
+
+    if (questionFour == true) {
+    } else {(setTime--);}
+
+    if (questionFive == true) {
+    } else {(setTime--);}
+}
+
+startBttn.addEventListener("click", generateQuiz);
+
 
 function setTime() {
     var timerInterval = setInterval(function() {
@@ -9,23 +40,25 @@ function setTime() {
 
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
-            secondMessage();
+            sendMessage();
         }
     }, 1000);
 }
 
-
-// addButton.addEventListener("click", function() {
-//     if ();
-// }
-// );
-function generateQuiz() {
-var questionOne = window.prompt("Javascript is a dynamic scripting language that enhances the user experience. Yes or No.");
-var questionTwo = window.prompt("You DO NOT use console logs to help explore your code. Yes or No.");
-var questionThree = window.prompt("An array is a variable with a list of values. Yes or No.");
-var questionFour = window.prompt("A 'for loop' executes a block of code so long as the condition of the statement is true. Yes or No.");
-var questionFive = window.prompt("The 'this' keyword is not confusing at all. Yes or No.");
-
-// For Loops //
-
+function sendMessage() {
+    timeEl.textContent = "Time is up!";
 }
+
+function saveInitials(){
+    localStorage.setItem("initials", initials);
+}
+
+function saveLastScore(){
+    localStorage.setItem("score", score);
+}
+
+saveButton.addEventListener("click", function(event) {
+event.preventDefault();
+saveInitials();
+saveLastScore();
+});
